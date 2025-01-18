a310b56679fd4c9704bbce2ce00c338d6e078da1
xy12180 <xy12180@gmail.com>
2025-01-17 20:46:32 -0800

    coins

---
+++ coin.py
@@ -0,0 +1,29 @@
+# Input: coins = [1,2,5], amount = 11
+# Output: 3
+# Explanation: 11 = 5 + 5 + 1
+
+# Input: coins = [2], amount = 3
+# Output: -1
+
+# Input: coins = [1], amount = 0
+# Output: 0
+
+def combos(coins, a):
+    if a == 0:
+        return 0
+
+    cs = pick(coins, a)
+    if not cs:
+        return None
+
+    try:
+        return min(1 + combos(coins, a-c) for c in cs)
+    except TypeError:
+        return -1
+
+def pick(coins, a):
+    return [c for c in coins if c <= a]
+
+print(combos([5,2,1], 11))
+print(combos([2], 3))
+print(combos([1], 0))
