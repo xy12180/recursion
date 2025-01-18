7da2a7a9076aca0b396049f5a2dd690337188e2a
xy12180 <xy12180@gmail.com>
2025-01-18 14:15:48 -0800

    coin: no need to use min() for dfs

--- coin.py
+++ coin.py
@@ -3,28 +3,33 @@
 # Explanation: 11 = 5 + 5 + 1
 
 # Input: coins = [2], amount = 3
-# Output: -1
+# Output: None
 
 # Input: coins = [1], amount = 0
 # Output: 0
 
-def combos(coins, a, level=0):
+def combos_r(coins, a):
     if a == 0:
         return 0
 
     cs = pick(coins, a)
     if not cs:
-        return -1 if level==0 else None
+        return None
 
     try:
-        return min(1 + combos(coins, a-c, level+1) for c in cs)
+        for c in cs:
+            return 1 + combos_r(coins, a-c)
     except TypeError:
-        return -1
+        return None
 
 def pick(coins, a):
     return [c for c in coins if c <= a]
 
-print(combos([1], 0))
-print(combos([2], 3))
-print(combos([2], 1))
-print(combos([5,2,1], 11))
+print(combos_r([1], 0))
+print(combos_r([2], 3))
+print(combos_r([2], 1))
+print(combos_r([5,2,1], 11))
+print(combos_r([5,2], 11))
+print(combos_r([5,3,1], 8))
+print(combos_r([5,3,1], 7))
+print(combos_r([5,3,1], 6))
