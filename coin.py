0f2d5db1a000a1c69de1fbe401023b05581dc3b5
xy12180 <xy12180@gmail.com>
2025-01-18 14:23:37 -0800

    coin: sort input and add tests

--- coin.py
+++ coin.py
@@ -9,6 +9,7 @@
 # Output: 0
 
 def combos_r(coins, a):
+    
     if a == 0:
         return 0
 
@@ -25,11 +26,20 @@ def combos_r(coins, a):
 def pick(coins, a):
     return [c for c in coins if c <= a]
 
-print(combos_r([1], 0))
-print(combos_r([2], 3))
-print(combos_r([2], 1))
-print(combos_r([5,2,1], 11))
-print(combos_r([5,2], 11))
-print(combos_r([5,3,1], 8))
-print(combos_r([5,3,1], 7))
-print(combos_r([5,3,1], 6))
+def combos(coins, a):
+    coins = sorted(coins, reverse=True)
+    return combos_r(coins, a)
+
+def test():
+    print("testing...")
+    assert combos([1], 0) == 0
+    assert combos([2], 3) == None
+    assert combos([2], 1) == None
+    assert combos([1,2,5], 11) == 3
+    assert combos([5,2], 11) == None
+    assert combos([1,3,5], 8) == 2
+    assert combos([1,3,5], 7) == 3
+    assert combos([1,3,5], 6) == 2
+
+test()
+print("passed")
